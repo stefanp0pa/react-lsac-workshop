a74b5a6554d139e0c114bac7e52d0b6a00ccbc23
StefanP-EQ <stefan.popa@equilobe.com>
2022-01-13 02:48:11 +0200

    Create basic components files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-
   );
 }
 
---
+++ src/atoms/BoardTitle/BoardTitle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function BoardTitle() {
+    return (
+        <div>BoardTitle</div>
+    );
+}
