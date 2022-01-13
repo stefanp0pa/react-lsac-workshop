020c9c451f5a293f917e979e0eb38fbd0dce16ef
Stefan <stefan.popa@equilobe.com>
2022-01-14 01:55:20 +0200

    Add item upgrade and downgrade icons

--- src/atoms/Item/Item.js
+++ src/atoms/Item/Item.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { ArrowRightIcon, XIcon } from '@heroicons/react/solid';
 
 export default function Item(props) {
+
+    const handleItemDowngrade = () => {
+        console.log("[Item] Downgrading item: " + props.title);
+        props.handleItemDowngrade(props.title);
+    }
+
+    const handleItemUpgrade = () => {
+        console.log("[Item] Upgrading item: " + props.title);
+        props.handleItemUpgrade(props.title);
+    }
+
     return (
-        <div className="text-white text-xl p-2 truncate">
-            {props.title}
+        <div className="text-white text-xl p-2 truncate flex flex-row items-center justify-between">
+            <span>{props.title}</span>
+            <div className="flex flex-row">
+                {props.boardTitle !== "Done" ? <ArrowRightIcon className="h-5 w-5 text-white" onClick={handleItemUpgrade}/> : "" }
+                {props.boardTitle !== "To Do" ? <XIcon className="h-5 w-5 text-white" onClick={handleItemDowngrade}/> : "" }
+            </div>
         </div>
     );
 }
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,6 +3,15 @@ import Item from '../../atoms/Item/Item';
 import BoardTitle from '../../atoms/BoardTitle/BoardTitle';
 
 export default function Board(props) {
+
+    const handleItemDowngrade = (title) => {
+        console.log("[Board] Downgrading item: " + title);
+    }
+
+    const handleItemUpgrade = (title) => {
+        console.log("[Board] Upgrading item: " + title);
+    }
+
     return (
         <div className="bg-blue-400 w-1/4 rounded-lg h-full">
             <div>
@@ -11,7 +20,7 @@ export default function Board(props) {
             <div>
                 {props.items.map((item, index) => {
                     return (
-                        <Item key={index} title={item} />
+                        <Item key={index} title={item} boardTitle = {props.title} handleItemDowngrade = {handleItemDowngrade} handleItemUpgrade = {handleItemUpgrade}/>
                     );
                 } )}
             </div>
