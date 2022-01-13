e891d2ad8e34d7861f9f82e290d7141f21d34da7
StefanP-EQ <stefan.popa@equilobe.com>
2022-01-13 03:19:24 +0200

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Workspace from './pages/Workspace/Workspace';
+import About from './pages/About/About';
+import Fun from './pages/Fun/Fun';
+
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/fun/:id">
+            <Fun/>
+          </Route>
+          <Route path="/">
+            <Workspace/>
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
