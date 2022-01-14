74a2990a33145e00abc0a48d140a3e0ce7bc57d2
Stefan <stefan.popa@equilobe.com>
2022-01-14 13:28:55 +0200

    Delete junk files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
-import Header from './components/Header/Header';
+import React from 'react';
 import Footer from './components/Footer/Footer';
 import Workspace from './pages/Workspace/Workspace';
-import About from './pages/About/About';
-import Fun from './pages/Fun/Fun';
-
-import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        {/* <Header /> */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/fun/:id">
-            <Fun/>
-          </Route>
-          <Route path="/">
-            <Workspace/>
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Workspace/>
+      <Footer/>
+    </div>
   );
 }
 
